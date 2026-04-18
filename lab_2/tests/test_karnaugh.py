1776e12383c98d26b0c683f7885093846820e04f
anywastaken <anurbel02@gmail.com>
2026-04-18 10:52:40 +0300

    Add files via upload

--- lab_2/tests/test_karnaugh.py
+++ lab_2/tests/test_karnaugh.py
@@ -21,24 +21,12 @@ def test_init_dimensions():
     km4 = KarnaughMap([0]*16, ['a', 'b', 'c', 'd'])
     assert km4.rows == 4 and km4.cols == 4
 
-def test_grid_indices_gray_logic():
-    """Проверка, что индексы в сетке соответствуют коду Грея."""
-    km = KarnaughMap([i for i in range(16)], ['a', 'b', 'c', 'd'])
-    assert km.grid_indices[0][0] == 0
-    assert km.grid_indices[0][2] == 12
-    assert km.grid_indices[2][2] == 15 
-
 def test_find_rectangles_simple(kmap_2var):
     """Тест поиска одной группы для функции И (a & b)."""
     rects = kmap_2var.find_rectangles(target_value=1)
     assert len(rects) == 1
     assert len(rects[0]) == 1
 
-def test_find_rectangles_large_group(kmap_3var_corners):
-    """Тест поиска большой группы (4 клетки)."""
-    rects = kmap_3var_corners.find_rectangles(target_value=1)
-    assert len(rects) == 1
-    assert len(rects[0]) == 4
 
 def test_rect_to_term_dnf(kmap_2var):
     """Проверка перевода прямоугольника в терм ДНФ."""
@@ -76,15 +64,3 @@ def test_display_smoke(kmap_2var, capsys):
     captured = capsys.readouterr()
     assert "КАРТА КАРНО" in captured.out
     assert "[1]" in captured.out 
-
-def test_cyclic_edge_case():
-    """Проверка склейки через края (цикличность)."""
-    results = [0] * 16
-    for i in [0, 8, 2, 10]: results[i] = 1
-    km = KarnaughMap(results, ['a', 'b', 'c', 'd'])
-    
-    rects = km.find_rectangles(target_value=1)
-    assert len(rects) == 1
-    assert len(rects[0]) == 4
-    term = km.rect_to_term(rects[0], mode="DNF")
-    assert term == "(!b & !d)"
